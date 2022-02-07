297821ac889e6627c0ff7de9adf0f469b95ba7d8
Hongdonggeon <ghd700@naver.com>
2022-02-07 19:18:33 +0900

    BO 2798 블랙잭

---
+++ src/bruteforce/Bo2798.java
@@ -0,0 +1,36 @@
+package bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bo2798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] cards = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(cards);
+
+        int answer = 0;
+
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (cards[i] + cards[j] + cards[k] > m || answer > cards[i] + cards[j] + cards[k]) {
+                        continue;
+                    }
+                    answer = cards[i] + cards[j] + cards[k];
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
