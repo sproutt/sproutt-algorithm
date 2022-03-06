0a545bbec69384e0a2aecdab4554fb866fd49133
saint6839 <saint6839@gmail.com>
2022-03-06 18:51:32 +0900

    refactor: Scanner, BufferedReader & StringTokenizer로 변경

--- src/week2/Baekjoon15650.java
+++ src/week2/Baekjoon15650.java
@@ -1,26 +1,29 @@
 package week2;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Baekjoon15650 {
     public static int N;
     public static int M;
     public static int[] numbers;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
 
-        N = scanner.nextInt();
-        M = scanner.nextInt();
+        N = Integer.parseInt(stringTokenizer.nextToken());
+        M = Integer.parseInt(stringTokenizer.nextToken());
 
         numbers = new int[M];
 
         backTracking(1, 0);
     }
-
     public static void backTracking(int at, int depth) {
-        if (depth == M) {
-            for (int number: numbers) {
+        if(depth == M) {
+            for (int number : numbers) {
                 System.out.print(number + " ");
             }
             System.out.println();
@@ -28,9 +31,8 @@ public class Baekjoon15650 {
         }
 
         for (int i = at; i <= N; i++) {
-
             numbers[depth] = i;
-            backTracking(i + 1, depth + 1);
+            backTracking(i+1, depth + 1);
         }
     }
 }
