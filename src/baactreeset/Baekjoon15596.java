e06bba9c90a5b0f54a97921f9775207239c65936
saint6839 <saint6839@gmail.com>
2022-08-17 21:43:38 +0900

    feat: 백준 15596 solved

---
+++ src/baactreeset/Baekjoon15596.java
@@ -0,0 +1,19 @@
+package baactreeset;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon15596 {
+    public static void main(String[] args) {
+    }
+
+    private static long sum(int[] a) {
+        long sum = 0;
+        for (int i : a) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+}
