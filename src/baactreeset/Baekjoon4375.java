ba45c70e062e5aa98f927c0f5d4dce8ed2f73eef
saint6839 <saint6839@gmail.com>
2022-09-04 16:45:51 +0900

    feat: 백준 4375 solved

---
+++ src/baactreeset/Baekjoon4375.java
@@ -0,0 +1,23 @@
+package baactreeset;
+
+import java.util.Scanner;
+
+public class Baekjoon4375 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        while(sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int num = 0;
+            for(int i = 1; ; i++) {
+                num = (num * 10) + 1;
+                num = num % n;
+                if(num == 0) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+}
